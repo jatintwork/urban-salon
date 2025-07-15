f33f797a301877ddfd6a1412b39b7ca477a1eaa7
jatintwork <106733086+jatintwork@users.noreply.github.com>
2025-07-15 23:44:35 +0530

    deleting code

--- Backend/urban_salon/base/urls.py
+++
@@ -1,9 +0,0 @@
-# base/urls.py
-from django.urls import path
-from base.base_views.SignUpView import SignupAPIView, DeleteUserAPIView, LoginAPIView
-
-urlpatterns = [
-    path('login/', LoginAPIView.as_view(), name='login'),
-    path('signup/', SignupAPIView.as_view(), name='signup'),
-    path('delete-user/', DeleteUserAPIView.as_view(), name='delete-user'),
-]
